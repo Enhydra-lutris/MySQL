4746d2f35cbb1eb5f421ba78b90ae9c8c349e59c
Enhydra-lutris <71213108+Enhydra-lutris@users.noreply.github.com>
2023-05-14 19:45:06 +0800

    20230514
    数据库修改

--- user.sql
+++ user.sql
@@ -3,7 +3,12 @@ CREATE TABLE IF NOT EXISTS `user`(
    `username` varchar(255) NOT NULL,
    `password` varchar(255) NOT NULL, 
    `mobile_phone` varchar(255) DEFAULT NULL,
+   `created_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   `updated_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
    PRIMARY KEY ( `id` )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into mypro.`user`(`id`,`username`,`password`,`mobile_phone`) VALUES ('1','alex','Yy970714!','13026367976');
+
+alter table `user` add created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+alter table `user` add updated_time TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
